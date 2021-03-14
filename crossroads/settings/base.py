cfda370d67886d13dfe10e369f334fdec9abcd1a
kyle v <kyle@verhoog.ca>
2021-03-14 12:57:41 -0400

    add django caching

--- crossroads/settings/base.py
+++ crossroads/settings/base.py
@@ -116,6 +116,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
 AUTH_USER_MODEL = "church.User"
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
