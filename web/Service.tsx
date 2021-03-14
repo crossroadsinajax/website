e56f6f8d16825dceadbbdc2f498f74510a53cbd7
kyle v <kyle@verhoog.ca>
2021-03-13 23:13:48 -0500

    Add gatherings page

--- web/Service.tsx
+++ web/Service.tsx
@@ -93,12 +93,42 @@ const Services: React.FC<ServicesProps> = () => {
   if (data && pages) {
     return (
       <Container>
-        <h1>Services</h1>
+        <h1>Sunday Gatherings</h1>
+        <p>
+          We gather every Sunday as a community to be encouraged and reminded
+          that we are not alone. Through praying, singing, sharing, learning,
+          discussing and laughing together, our Sunday gatherings connect us to
+          God and to each other, giving us a boost for the week to come!
+        </p>
+        <p>
+          Some of the values that mark our gatherings include:
+          <br />
+          <br />
+          <b>Informal:</b> Come as you are, not as you are ‘expected’ to be!
+          <br />
+          <b>Participatory:</b> Feel free to join the conversation, your voice
+          is welcome!
+          <br />
+          <b>Authentic:</b> We want it to be real, not religious!
+          <br />
+          <b>Relevant:</b> Connecting God to the real stuff of your life!
+          <br />
+          <br />
+          During COVID-19 we are meeting online only, through our website at
+          10:30 a.m. EST on Sunday mornings, every Sunday morning! Please feel
+          free to join us using the links on the Service links page or contact
+          kyle@verhoog.ca for access! Our Ministry Centre is closed, but our
+          ministry continues. In partnership with Johanne’s House, Southside
+          Foodbank, John Howard Society, and other tremendous organizations, we
+          seek to serve the Ajax community uninterrupted.
+        </p>
         <React.Fragment>
           <ul>
             {pages.map((page: any, i: number) => (
               <li key={i}>
-                <Link to={`/service/${page.node.slug}`}>{page.node.title}</Link>
+                <Link to={`/gathering/${page.node.slug}`}>
+                  {page.node.title}
+                </Link>
               </li>
             ))}
           </ul>
