cfa3fff0c38410fde766a7664cbc7d2960fdfd32
kyle verhoog <kyle@verhoog.ca>
2021-04-14 03:24:20 -0400

    configure RUM

--- crossroads/settings/dev.py
+++ crossroads/settings/dev.py
@@ -9,6 +9,9 @@ SECRET_KEY = "(n(9#@@keu0+$2l6fo6lnjrugimt@g5sn&&*2wnslfk3-_djg#"
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
+RUM_APP_ID = "23a0f3c3-09fc-4c27-ae76-d147e820a3ac"
+RUM_CLIENT_TOKEN = "pub7c18ec5932f220e2a42e9ca8a3e1b7c1"
+
 try:
     from .local import *  # noqa
 except ImportError:
