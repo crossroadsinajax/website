0b2f1117e996bcdf34e45d89f19592f35112986a
kyle verhoog <kyle@verhoog.ca>
2021-04-17 23:28:19 -0400

    implement chat scroll pausing

--- crossroads/settings/dev.py
+++ crossroads/settings/dev.py
@@ -1,14 +1,13 @@
 from .base import *  # noqa
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "(n(9#@@keu0+$2l6fo6lnjrugimt@g5sn&&*2wnslfk3-_djg#"
 
-# SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
+
+# RUM (Real user monitoring) settings
 RUM_APP_ID = "23a0f3c3-09fc-4c27-ae76-d147e820a3ac"
 RUM_CLIENT_TOKEN = "pub7c18ec5932f220e2a42e9ca8a3e1b7c1"
 
