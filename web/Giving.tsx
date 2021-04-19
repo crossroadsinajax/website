b9ce8a7b25f75e0a28929738eef952ef076ef1d3
kyle verhoog <kyle@verhoog.ca>
2021-04-18 23:11:33 -0400

    style giving page

--- web/Giving.tsx
+++ web/Giving.tsx
@@ -9,54 +9,81 @@ const Giving: React.FC<GivingProps> = () => {
   return (
     <Container>
       <Title text="Giving" />
-      <h1>Giving</h1>
-      <p>
-        There are multiple ways to donate to support Crossroads and its
-        ministries
-      </p>
+      <div className="text-center">
+        <h1>Giving</h1>
+        <p className="lead">
+          There are multiple ways to donate to support Crossroads and its
+          ministries
+        </p>
+      </div>
+      <p></p>
 
-      <h2>Paypal</h2>
-      <form
-        action="https://www.paypal.com/cgi-bin/webscr"
-        method="post"
-        target="_top"
-      >
-        <input type="hidden" name="cmd" value="_s-xclick" />
-        <input type="hidden" name="hosted_button_id" value="4QK9PVMWKV9ZE" />
-        <input
-          type="image"
-          src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-          name="submit"
-          title="PayPal - The safer, easier way to pay online!"
-          alt="Donate with PayPal button"
-        />
-        <img
-          alt=""
-          src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
-          width="1"
-          height="1"
-        />
-      </form>
+      <Container>
+        <div className="card-deck mb-3 text-center">
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4>Paypal</h4>
+            </div>
+            <div className="card-body">
+              <form
+                className="mt-2"
+                action="https://www.paypal.com/cgi-bin/webscr"
+                method="post"
+                target="_top"
+              >
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input
+                  type="hidden"
+                  name="hosted_button_id"
+                  value="4QK9PVMWKV9ZE"
+                />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                  name="submit"
+                  title="PayPal - The safer, easier way to pay online!"
+                  alt="Donate with PayPal button"
+                />
+                <img
+                  alt=""
+                  src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+                  width="1"
+                  height="1"
+                />
+              </form>
+            </div>
+          </div>
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4>PAR</h4>
+            </div>
+            <div className="card-body">
+              <p>Pre-authorized Remittance</p>
+              <p>Contact Lynn for more details.</p>
+            </div>
+          </div>
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4>E-Transfer</h4>
+            </div>
+            <div className="card-body">
+              <ul className="list-unstyled mb-4">
+                <li>
+                  Send to{" "}
+                  <a href="mailto:lynn@crossroadsinajax.org">
+                    lynn@crossroadsinajax.org
+                  </a>
+                </li>
+                <li>
+                  Notify Lynn via email of the amount and Ministry focus if any.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
 
-      <h2>Pre-authorized Remittance (PAR)</h2>
-      <p>Contact the administrator (see below) for more details</p>
-
-      <h2>Interac E-Transfer</h2>
-      <ul>
-        <li>
-          Sign in to your preferred bank&apos;s Online Banking and click
-          &quot;send E-Transfer&quot;
-        </li>
-        <li>
-          Send to{" "}
-          <a href="mailto:lynn@crossroadsinajax.org">
-            lynn@crossroadsinajax.org
-          </a>
-        </li>
-        <li>Notify Lynn via email of the amount and Ministry focus if any.</li>
-      </ul>
-
-      <h2>Contact Administrator</h2>
+      <h2>Administrator Contact</h2>
       <p>
         Lynn Jackson
         <br />
