f16d508d5ad9a05f7d210a59f8eb2578f8553eac
Kyle Verhoog <kyle@verhoog.ca>
2021-06-14 04:05:29 -0400

    Migrate database to postgres

--- crossroads/settings/base.py
+++ crossroads/settings/base.py
@@ -120,8 +120,12 @@ TEMPLATES = [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crossroads",
+        "USER": "postgres",
+        "PASSWORD": "password",
     }
 }
 
