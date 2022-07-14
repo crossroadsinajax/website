45c18da65ccf111cc9f2d3aab2f6e971e6ddefec
Kyle Verhoog <kyle@verhoog.ca>
2022-07-13 20:56:38 -0400

    Patch integrations

--- crossroads/dd.py
+++ crossroads/dd.py
@@ -3,7 +3,9 @@ from datadog import DDClient, DDConfig
 ddconfig = DDConfig(
     version_use_git=True,
     profiling_enabled=True,
+    tracing_enabled=True,
 )
 ddclient = DDClient(config=ddconfig)
+ddclient.patch(["django", "psycopg", "sqlite3"])
 
 DatadogLogHandler = ddclient.LogHandler
